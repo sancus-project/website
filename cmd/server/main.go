33e3ad192f7b70f267f4d7adc6e4a047b30d36a6
Alejandro Mery <amery@geeks.cl>
2021-02-27 01:10:01 +0000

    server: replace github.com/rs/seamless with github.com/cloudflare/tableflip
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,24 +7,25 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
-	reuseport "github.com/kavu/go_reuseport"
+	"github.com/cloudflare/tableflip"
 	"github.com/pborman/getopt/v2"
-	"github.com/rs/seamless"
 
 	"github.com/amery/go-webpack-starter/assets"
 )
 
 var (
 	waitStarted     = time.Second // wait one second before considering it started
-	defaultPidFile  = "/tmp/reuseport.pid"
+	defaultPidFile  = "/tmp/tableflip.pid"
 	defaultPort     = 8080
 	defaultGraceful = 60 * time.Second
 
 	devFlag         = getopt.BoolLong("dev", 'd', "Don't hashify static files")
 	portListen      = getopt.Uint16Long("port", 'p', uint16(defaultPort), "HTTP port to listen")
-	pidFile         = getopt.StringLong("pid", 'f', defaultPidFile, "Seemless restart PID file")
+	pidFile         = getopt.StringLong("pid", 'f', defaultPidFile, "Path to PID file")
 	gracefulTimeout = getopt.DurationLong("graceful", 't', defaultGraceful, "Maximum duration to wait for in-flight requests")
 )
 
@@ -40,7 +41,11 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
+	// check argments
 	getopt.Parse()
+
+	// setup
+	log.SetPrefix(fmt.Sprintf("pid:%d ", os.Getpid()))
 
 	listenAddr := fmt.Sprintf(":%v", *portListen)
 
@@ -54,36 +59,56 @@ func main() {
 		s.Handler = assets.Files.Handler(true, s.Handler)
 	}
 
-	if !*devFlag && *gracefulTimeout > 0 {
+	if *gracefulTimeout > 0 {
 		// Graceful restart mode
-		seamless.Init(*pidFile)
+		upg, err := tableflip.New(tableflip.Options{
+			PIDFile: *pidFile,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer upg.Stop()
 
-		l, err := reuseport.Listen("tcp", listenAddr)
+		// attempt upgrade on SIGUSR2
+		go func() {
+			sig := make(chan os.Signal, 1)
+			signal.Notify(sig, syscall.SIGUSR2)
+			for range sig {
+				if err := upg.Upgrade(); err != nil {
+					log.Println("Upgrade failed:", err)
+				}
+			}
+		}()
+
+		// listen service port
+		l, err := upg.Listen("tcp", listenAddr)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		seamless.OnShutdown(func() {
-			ctx, cancel := context.WithTimeout(context.Background(), *gracefulTimeout)
-			defer cancel()
+		log.Print(fmt.Sprintf("Listening %s", s.Addr))
 
-			if err := s.Shutdown(ctx); err != nil {
-				log.Print("Graceful shutdown timeout, force closing")
-				s.Close()
-			}
-		})
-
+		// starter servicing
 		go func() {
-			time.Sleep(waitStarted)
-			seamless.Started()
+			err := s.Serve(l)
+			if err != nil && err != http.ErrServerClosed {
+				log.Fatal(err)
+			}
 		}()
 
-		err = s.Serve(l)
-		if err != nil && err != http.ErrServerClosed {
+		if err = upg.Ready(); err != nil {
 			log.Fatal(err)
 		}
+		<-upg.Exit()
 
-		seamless.Wait()
+		// graceful shutdown timeout
+		time.AfterFunc(*gracefulTimeout, func() {
+			log.Println("Graceful shutdown timed out")
+			os.Exit(1)
+		})
+
+		// Wait for connections to drain.
+		s.Shutdown(context.Background())
 	} else {
 		l, err := net.Listen("tcp", s.Addr)
 		if err != nil {
--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.15
 
 require (
 	github.com/amery/file2go v0.9.0
+	github.com/cloudflare/tableflip v1.2.2
 	github.com/cortesi/modd v0.0.0-20210222043654-cbdcc23af7d5 // indirect
-	github.com/kavu/go_reuseport v1.5.0
 	github.com/pborman/getopt/v2 v2.1.0
-	github.com/rs/seamless v1.0.0
+	golang.org/x/sys v0.0.0-20210220050731-9a76102bfb43 // indirect
 )
