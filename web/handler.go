f1cf354c25b887ed79f765b549e01b6c487132bc
Alejandro Mery <amery@geeks.cl>
2021-04-11 18:18:30 +0000

    web: fallback to 302 https://github.com/sancus-project
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- web/handler.go
+++ web/handler.go
@@ -1,33 +1,9 @@
 package web
 
 import (
-	"log"
 	"net/http"
-	"os"
-	"time"
-
-	"github.com/sancus-project/website/html"
 )
 
-type View struct {
-	Pid int
-}
-
 func HandleIndex(w http.ResponseWriter, r *http.Request) {
-	if d := r.URL.Query().Get("delay"); d != "" {
-		if delay, err := time.ParseDuration(d); err == nil {
-			time.Sleep(delay)
-		}
-	}
-
-	w.WriteHeader(http.StatusOK)
-
-	vd := View{
-		Pid: os.Getpid(),
-	}
-
-	err := html.View(r, "index", vd).Render(w, r)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	http.Redirect(w, r, "https://github.com/sancus-project", 302)
 }
